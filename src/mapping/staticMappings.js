91022f146347a56acafed538463e656e03cfadef
Umran Hussein <umran.hussein@gmail.com>
2019-05-28 13:46:06 -0700

    removed redundant mappings

--- src/mapping/staticMappings.js
+++ src/mapping/staticMappings.js
@@ -3,15 +3,7 @@ module.exports = {
     type: 'keyword',
     index: true
   },
-  '@version': {
-    type: 'integer',
-    index: true
-  },
   '@lastModified': {
-    type: 'date',
-    index: true
-  },
-  '@lastCommitted': {
     type: 'date',
     index: true
   }
